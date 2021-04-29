29736eb6079cc8ff32d8c82cc18b367f948d3df1
chris12344y <shadowstar14523@gmail.com>
2021-04-29 13:03:45 -0500

    finally figured out how to do the title and image behind it.

--- GroupProjectV4/Site1.Master.designer.cs
+++ GroupProjectV4/Site1.Master.designer.cs
@@ -33,6 +33,15 @@ namespace GroupProjectV4
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
         /// UserNameTxtBox control.
         /// </summary>
         /// <remarks>
@@ -121,15 +130,6 @@ namespace GroupProjectV4
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Upload;
-
-        /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
