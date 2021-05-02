5ccbfe96c511ce8cca6c3dc3252024579f996ead
Aidan Becker <49326603+abecker96@users.noreply.github.com>
2021-05-02 15:49:03 -0500

    terrible idea

--- GroupProjectV4/ViewMaps.aspx.designer.cs
+++ GroupProjectV4/ViewMaps.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace GroupProjectV4
 
     public partial class ViewMaps
     {
-
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
